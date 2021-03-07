1defd647c31cc6fc1fc6b1b484a5e1adf6e55119
zecarloz <josecarlos2503@hotmail.com>
2021-03-06 22:11:01 -0300

    adição projeto 1.1

--- projeto1.1/index.js
+++ projeto1.1/index.js
@@ -3,29 +3,15 @@ const app = express();
 
 // Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine','ejs');
+app.use(express.static('public'));
 
-app.get("/:nome/:lang",(req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-
-    var produtos = [
-        {nome: "Doritos",preco: 3.14},
-        {nome: "Coca-cola",preco:5},
-        {nome: "Leite",preco:1.45},
-        {nome: "Carne", preco:15},
-        {nome: "Redbull", preco: 6},
-        {nome: "Nescau", preco: 4}
-    ]
-
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do programador",
-        inscritos: 8040,
-        msg: exibirMsg,
-        produtos: produtos
+app.get("/",(req, res) => {
+    res.render("index");     
     });
-});
+
+app.get("/perguntar",(req,res)=>{
+        res.render("perguntar");
+    });
+    
 
 app.listen(8080,()=>{console.log("App rodando!");});
