aaa2bcc80b7f7c0961abc4e49d5a36c91059ec2d
zecarloz <josecarlos2503@hotmail.com>
2021-03-08 19:55:39 -0300

    update projeto 1.1

--- projeto1.1/index.js
+++ projeto1.1/index.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine','ejs');
 app.use(express.static('public'));
-
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// rotas
 app.get("/",(req, res) => {
     res.render("index");     
     });
 
 app.get("/perguntar",(req,res)=>{
         res.render("perguntar");
-    });
-    
+    })
+
+app.post("/salvarpergunta",(req,res)=>{
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulario recebido !"+ "<br>" + "titulo :" + titulo + " " +"<br>"+ "descricao : " + descricao);
+});   
 
 app.listen(8080,()=>{console.log("App rodando!");});
