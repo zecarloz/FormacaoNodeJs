f3ccb63ae7321afaa7a2e9a7fb0a8a7395bc21b0
Jose carlos <josecarlos2503@hotmail.com>
2021-03-13 17:36:31 -0300

    Update projeto 1.0.0

--- projeto v1.0.0/index.js
+++ projeto v1.0.0/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const perguntaModel = require('./database/Pergunta');
+// Database
+connection
+    .authenticate()
+    .then(()=>{
+        console.log("Conexão feita com o banco de dados!")
+    })
+    .catch((msgErro)=>{
+        console.log(msgErro);  
+    })
 
 // Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine','ejs');
