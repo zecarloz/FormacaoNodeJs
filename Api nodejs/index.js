3cac3368b5d21392bb4b3075b3a8d4296a38ffcc
Jose carlos <josecarlos2503@hotmail.com>
2021-07-07 17:49:48 -0300

    Projeto Api com nodejs

--- Api nodejs/index.js
+++ Api nodejs/index.js
@@ -10,8 +10,29 @@ app.use(bodyParser.urlencoded({ extend: false}))
 app.use(express.json())
 app.use(cors())
 
+let db = [
+    {'1' : {Nome: 'cliente 1', Idade:'20'}},
+    {'2' : {Nome: 'cliente 2', Idade:'20'}},
+    {'3' : {Nome: 'cliente 3', Idade:'20'}}
+]
+// buscar dados
+app.get('/' , (req,res) => {
+    return res.json(db)
+})
+// inserir dados
+app.post('/add' , (req,res) => {
+   const body = req.body
+
+   if(!body)
+    return res.status(400).end()
+  db.push(body)
+  return res.json(body)  
+})
+
+
+
 app.listen(21262, () => {
     console.log('Express started at http://localhost:21262')
 })
 
-//22''
+//33''
